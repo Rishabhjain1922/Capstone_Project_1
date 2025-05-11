b556d890772ebc326a90ced99460f8c33faed4b7
Rishabh Jain <141677277+Rishabhjain1922@users.noreply.github.com>
2025-05-11 13:44:04 +0530

    Delete Evert-Organizer directory

--- Evert-Organizer/src/main/java/Capstone_Project_1/Evert/Organizer/repository/AttendeeRepository.java
+++
@@ -1,9 +0,0 @@
-package Capstone_Project_1.Evert.Organizer.repository;
-import Capstone_Project_1.Evert.Organizer.model.Attendee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-// This interface extends JpaRepository to provide CRUD operations for the Attendee entity.
-public interface AttendeeRepository extends JpaRepository<Attendee, Long> {
-    Attendee findByUsername(String username);
-}
