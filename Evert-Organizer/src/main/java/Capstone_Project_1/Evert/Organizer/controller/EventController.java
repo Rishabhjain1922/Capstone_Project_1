2200d895a70a86827d14b3efa0397748fba81b0a
Rishabhjain1922 <Rishabhjain1922@gmail.com>
2025-06-02 13:45:04 +0530

    Event Controller Cors change

--- Evert-Organizer/src/main/java/Capstone_Project_1/Evert/Organizer/controller/EventController.java
+++ Evert-Organizer/src/main/java/Capstone_Project_1/Evert/Organizer/controller/EventController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/events")
 public class EventController {
